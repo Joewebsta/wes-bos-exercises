fd6eac8a33ddd68b8b6f22d5d8d907ffd876b6e4
Joe Webster <joseph.scott.webster@gmail.com>
2020-05-26 18:26:09 -0400

    Update Result area text when key is pressed in text area

--- beginner-javascript/sarcastic-text/text.js
+++ beginner-javascript/sarcastic-text/text.js
@@ -1,1 +1,10 @@
-console.log('it works!');
+const textArea = document.querySelector('textarea');
+
+const results = document.querySelector('.result');
+
+console.log(textArea);
+
+textArea.addEventListener('keydown', e => {
+  const textAreaText = e.currentTarget.value;
+  results.textContent = textAreaText;
+});
